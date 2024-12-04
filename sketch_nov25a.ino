a2ae064987f938063f78da15a2463c2daff78a07
SmartestLizard <152944233+SmartestLizard@users.noreply.github.com>
2024-12-03 17:07:59 -0800

    Add files via upload

--- sketch_nov25a.ino
+++ sketch_nov25a.ino
@@ -13,6 +13,12 @@ volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
+//ADC registers
+volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
+volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
+volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
+volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+
 // Registers for LEDs and vent left/right button and start, reset, and stop buttons
 // Define Port J Register Pointers
 volatile unsigned char* port_j = (unsigned char*) 0x105; 
@@ -54,11 +60,10 @@ float humi;
 
 //threshold values
 const int tempthreshold = 75; //adjust
-const int waterthreshold = 12; //adjust
+const int waterthreshold = 20; //adjust
 
 
-//remove//////////////////////////////////////////////////////////////////////////////////////////////////////
-int water = 11;
+unsigned int water = 0;
 
 
 void setup() {
@@ -67,6 +72,9 @@ void setup() {
   dht.begin(); // initialize the temp and humidity sensor
   temp = dht.readTemperature(true);
   humi  = dht.readHumidity();
+
+  adc_init();
+  
   //interrupts
   *ddr_e &= 0x01;//PE0
   *ddr_e &= 0x02;//PE1
@@ -105,6 +113,7 @@ void loop() {
     interruptTime = false;
     U0putchar(timeReport());
   }
+  water = adc_read(0);
   DateTime now = rtc.now();
   if(now.day() == 0){
     humi  = dht.readHumidity();
@@ -201,6 +210,47 @@ void U0init(unsigned long U0baud)
  *myUCSR0C = 0x06;
  *myUBRR0  = tbaud;
 }
+
+void adc_init()
+{
+  // setup the A register
+  *my_ADCSRA |= 0b10000000; // set bit   7 to 1 to enable the ADC
+  *my_ADCSRA &= 0b11011111; // clear bit 6 to 0 to disable the ADC trigger mode
+  *my_ADCSRA &= 0b11110111; // clear bit 5 to 0 to disable the ADC interrupt
+  *my_ADCSRA &= 0b11111000; // clear bit 0-2 to 0 to set prescaler selection to slow reading
+  // setup the B register
+  *my_ADCSRB &= 0b11110111; // clear bit 3 to 0 to reset the channel and gain bits
+  *my_ADCSRB &= 0b11111000; // clear bit 2-0 to 0 to set free running mode
+  // setup the MUX Register
+  *my_ADMUX  &= 0b01111111; // clear bit 7 to 0 for AVCC analog reference
+  *my_ADMUX  |= 0b01000000; // set bit   6 to 1 for AVCC analog reference
+  *my_ADMUX  &= 0b11011111; // clear bit 5 to 0 for right adjust result
+  *my_ADMUX  &= 0b11100000; // clear bit 4-0 to 0 to reset the channel and gain bits
+}
+unsigned int adc_read(unsigned char adc_channel_num)
+{
+  // clear the channel selection bits (MUX 4:0)
+  *my_ADMUX  &= 0b11100000;
+  // clear the channel selection bits (MUX 5)
+  *my_ADCSRB &= 0b11110111;
+  // set the channel number
+  if(adc_channel_num > 7)
+  {
+    // set the channel selection bits, but remove the most significant bit (bit 3)
+    adc_channel_num -= 8;
+    // set MUX bit 5
+    *my_ADCSRB |= 0b00001000;
+  }
+  // set the channel selection bits
+  *my_ADMUX  += adc_channel_num;
+  // set bit 6 of ADCSRA to 1 to start a conversion
+  *my_ADCSRA |= 0x40;
+  // wait for the conversion to complete
+  while((*my_ADCSRA & 0x40) != 0);
+  // return the result in the ADC data register
+  return *my_ADC_DATA;
+}
+
 String timeReport()
 {
   DateTime now = rtc.now();
